062571b78ae1c14d76c8c92ac8ec5e76ef7bcce4
unknown <josecanahuate05@gmail.com>
2024-04-03 16:15:11 -0500

    slug getroutekeyname añadido

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,6 +12,11 @@ class Category extends Model
         //asignacion masiva admin.categories.create
         protected $fillable = ['name', 'slug']; 
 
+        public function getRouteKeyName()
+        {
+            return "slug";
+        }
+
         //relacion uno a muchos entre usuario y post
         public function posts(){    
             return $this->hasMany(Post::class);  
